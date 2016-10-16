a6291aee5c8b2cf67c5ebd35a2adbf79c6e8557a
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-16 00:37:39 -0400

    Overload operator<< for Resource enum

--- src/main/Resource.hpp
+++ src/main/Resource.hpp
@@ -1,10 +1,26 @@
 #ifndef RESOURCE_HPP
 #define RESOURCE_HPP
 
+#include <iostream>
+#include <map>
+#include <string>
+
 enum class Resource {
     COM_PRODUCT,
     IND_PRODUCT,
     PERSON
 };
+
+static std::map<Resource, std::string> resourceToString = {
+    {Resource::COM_PRODUCT, "COM_PRODUCT"},
+    {Resource::IND_PRODUCT, "IND_PRODUCT"},
+    {Resource::PERSON, "PERSON"}
+};
+
+inline std::ostream& operator<<(std::ostream& stream, const Resource& resource) {
+    stream << resourceToString[resource];
+
+    return stream;
+}
 
 #endif
