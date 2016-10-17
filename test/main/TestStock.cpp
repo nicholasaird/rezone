ef969b836e285512913ba43a9a4d4dcc3cbefc66
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-17 16:05:13 -0400

    Add tests for (*)

--- test/main/TestStock.cpp
+++ test/main/TestStock.cpp
@@ -468,6 +468,62 @@ TEST(TestStock, APlusEqualMinusAShouldMakeEmpty2) {
     ASSERT_EQ(Stock(), stockA);
 }
 
+TEST(TestStock, EmptyTimesZeroReturnsEmpty) {
+    Stock stockA;
+
+    Stock result = stockA * 0;
+
+    ASSERT_EQ(Stock(), result);
+}
+
+TEST(TestStock, EmptyTimesOneReturnsEmpty) {
+    Stock stockA;
+
+    Stock result = stockA * 1;
+
+    ASSERT_EQ(Stock(), result);
+}
+
+TEST(TestStock, EmptyTimesTwoReturnsEmpty) {
+    Stock stockA;
+
+    Stock result = stockA * 2;
+
+    ASSERT_EQ(Stock(), result);
+}
+
+TEST(TestStock, ATimesZeroReturnsEmpty) {
+    Stock stockA({{Resource::PERSON, 1}});
+
+    Stock result = stockA * 0;
+
+    ASSERT_EQ(Stock(), result);
+}
+
+TEST(TestStock, ATimesOneReturnsA) {
+    Stock stockA({{Resource::PERSON, 1}});
+
+    Stock result = stockA * 1;
+
+    ASSERT_EQ(Stock({{Resource::PERSON, 1}}), result);
+}
+
+TEST(TestStock, ATimesTwoReturnsDouble) {
+    Stock stockA({{Resource::PERSON, 1}});
+
+    Stock result = stockA * 2;
+
+    ASSERT_EQ(Stock({{Resource::PERSON, 2}}), result);
+}
+
+TEST(TestStock, ATimesTwoReturnsDouble2) {
+    Stock stockA({{Resource::PERSON, 1}, {Resource::ELECTRICITY, 1}});
+
+    Stock result = stockA * 2;
+
+    ASSERT_EQ(Stock({{Resource::PERSON, 2}, {Resource::ELECTRICITY, 2}}), result);
+}
+
 TEST(TestStock, EmptyAddEmptyShouldReturnEmpty) {
     Stock stockA;
     Stock stockB;
