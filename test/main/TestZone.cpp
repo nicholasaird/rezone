961ca420697e0f67138638481a1319755a6bbaf2
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-17 13:35:31 -0400

    Removed Zone test that were unclear
    They were testing for initial PERSON production to be 0, when it should actually be something.

--- test/main/TestZone.cpp
+++ test/main/TestZone.cpp
@@ -3,21 +3,3 @@
 #include "Resource.h"
 #include "Zone.h"
 #include "Stock.h"
-
-TEST(TestZone, InitialPopProducedShouldBeZero) {
-    Zone zone;
-
-    Stock result = zone.getProductionTotal();
-    Stock expected({{Resource::PERSON, 0}});
-
-    ASSERT_EQ(expected, result);
-}
-
-TEST(TestZone, InitialPopAvailableShouldBeZero) {
-    Zone zone;
-
-    Stock result = zone.getProductionUnused();
-    Stock expected({{Resource::PERSON, 0}});
-
-    ASSERT_EQ(expected, result);
-}
