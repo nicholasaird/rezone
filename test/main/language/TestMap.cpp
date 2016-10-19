44cdb1622f551163608818a3beb06533211a39c6
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-18 20:12:18 -0400

    Test iteration by value over std::map

--- test/main/language/TestMap.cpp
+++ test/main/language/TestMap.cpp
@@ -11,3 +11,13 @@ TEST(TestMap, WhenIterationByReferenceShouldChangeValue) {
 
     ASSERT_EQ(2, map[1]);
 }
+
+TEST(TestMap, WhenIterationByValueShouldNotChangeValue) {
+    std::map<int, int> map({{1, 1}});
+
+    for(auto pair : map) {
+        pair.second = 2;
+    }
+
+    ASSERT_EQ(1, map[1]);
+}
