880ac69411f5bb93fcb1a779dfbea99a78080822
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-18 20:11:33 -0400

    Test iteration by reference over std::map

---
+++ test/main/language/TestMap.cpp
@@ -0,0 +1,13 @@
+#include "gtest/gtest.h"
+
+#include <map>
+
+TEST(TestMap, WhenIterationByReferenceShouldChangeValue) {
+    std::map<int, int> map({{1, 1}});
+
+    for(auto& pair : map) {
+        pair.second = 2;
+    }
+
+    ASSERT_EQ(2, map[1]);
+}
