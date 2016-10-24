7db5f80a5be9bb74eb859e5dea5a5a2a822c0428
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-10-24 17:03:01 -0400

    Add sudo for apt-get, and ppa for g++

--- travisInstallCompiler.sh
+++ travisInstallCompiler.sh
@@ -1,12 +1,16 @@
 set -e
 
 if [ $COMPILER = "clang++-3.9" ]; then
+    echo "Installing clang++-3.9..."
     sudo add-apt-repository "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.9 main"
-    apt-get update
-    apt-get install clang-3.9 lldb-3.9
+    sudo apt-get update
+    sudo apt-get install clang-3.9 lldb-3.9
 fi
 
 if [ $COMPILER = "g++-5" ]; then
+    echo "Installing g++-5..."
+    sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+    sudo apt-get update
     sudo apt-get install g++-5
 fi
 
