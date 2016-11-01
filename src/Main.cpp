9ba7d67f8ae57e898ba5bfa97000663253ca270c
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-01 18:58:09 -0400

    Use new prompt and command classes

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,120 +1,55 @@
 #include <iomanip>
 #include <iostream>
-#include <memory>
+#include <memory>  // shared pointer
 #include <string>
 #include <sstream>  // istringstream
 #include <iterator>  // istream_iterator
 
+#include "docopt.h"
 #include "main/Grid.h"
 #include "main/Zone.h"
+#include "main/Map.h"
+#include "main/MapSelection.h"
+#include "main/Command/CommandPrompt.h"
+#include "main/Command/SelectCommand.h"
+#include "main/Command/QuitCommand.h"
 
-void resetZones(Grid<std::shared_ptr<Zone> >& zones);
-void printZones(Grid<std::shared_ptr<Zone> >& zones);
 void run();
-void loop(Grid<std::shared_ptr<Zone> >& zones, Stock& relief);
-std::string getCommand();
-void executeCommand(std::string command);
-void updateZones(Grid<std::shared_ptr<Zone> >& zones, Stock& relief);
+void loop();
 
-bool running = true;
+std::shared_ptr<bool> running = std::make_shared<bool>(true);
+static const char USAGE[] =
+R"(Rezone
 
-int main() {
+    Usage:
+        select
+        unselect
+)";
+std::shared_ptr<Map> map;
+std::shared_ptr<MapSelection> mapSelection;
+Stock relief;
+
+int main(int argc, char* argv[]) {
     run();
 
     return 0;
 }
 
-void resetZones(Grid<std::shared_ptr<Zone> >& zones) {
-    for(int x = 0; x < zones.getWidth(); x++) {
-        for(int y = 0; y < zones.getHeight(); y++) {
-            zones(x, y) = std::make_shared<Zone>("Empty", StockPair(Stock(), Stock()));
-        }
-    }
-}
-
-void printZones(Grid<std::shared_ptr<Zone> >& zones) {
-    std::cout << "Map" << std::endl;
-
-    for(int x = 0; x < zones.getWidth(); x++) {
-        std::cout << "+------------";
-    }
-
-    std::cout << "+" << std::endl;
-
-    for(int x = 0; x < zones.getWidth(); x++) {
-
-        for(int y = 0; y < zones.getHeight(); y++) {
-            std::cout << "| ";
-
-            Zone& zone = *zones(x, y);
-            std::string nameAbbreviation = zone.getName().substr(0, 3);
-            Stock production = zone.getTaken();
-            int takeAmount = production.sum();
-            std::cout << nameAbbreviation << ": " << std::right << std::setw(5) << std::to_string(takeAmount);
-            std::cout << " ";
-        }
-
-        std::cout << "|" << std::endl;
-
-        for(int x = 0; x < zones.getWidth(); x++) {
-            std::cout << "+------------";
-        }
-        std::cout << "+" << std::endl;
-    }
-}
-
 void run() {
-    Grid<std::shared_ptr<Zone> > zones(4, 4);
-    resetZones(zones);
+    map = std::make_shared<Map>(4, 4);
+    mapSelection = std::make_shared<MapSelection>();
+    relief = Stock({{Resource::PERSON, 10}});
 
-    Stock relief({{Resource::PERSON, 10}});
-
-    loop(zones, relief);
+    loop();
 }
 
-void loop(Grid<std::shared_ptr<Zone> >& zones, Stock& relief) {
-    while(running) {
-        printZones(zones);
-        std::cout << std::endl;
+void loop() {
+    CommandPrompt commandPrompt;
+    commandPrompt.addCommand("select", std::make_shared<SelectCommand>(map, mapSelection));
+    commandPrompt.addCommand("q", std::make_shared<QuitCommand>(running));
+    commandPrompt.addCommand("quit", std::make_shared<QuitCommand>(running));
 
-        std::string command = getCommand();
-
-        executeCommand(command);
-
-        updateZones(zones, relief);
-    }
-}
-
-std::string getCommand() {
-    std::string command;
-    std::cout << "> ";
-    getline(std::cin, command);
-
-    return command;
-}
-
-void executeCommand(std::string command) {
-    std::istringstream iss(command);
-    std::istream_iterator<std::string> beg(iss);
-    std::istream_iterator<std::string> end;
-    std::vector<std::string> tokens(beg, end);
-
-    if(tokens.size() == 0) {
-        return;
-    }
-
-    if(tokens[0] == "q") {
-        std::cout << "quit" << std::endl;
-        running = false;
-    }
-}
-
-void updateZones(Grid<std::shared_ptr<Zone> >& zones, Stock& relief) {
-    for(int x = 0; x < zones.getWidth(); x++) {
-        for(int y = 0; y < zones.getHeight(); y++) {
-            Zone& zone = *zones(x, y);
-            zone.updateCap(relief);
-            zone.updateProduction();
-        }
+    while(*running) {
+        commandPrompt.prompt();
     }
 }
