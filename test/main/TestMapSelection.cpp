845361a2b50a2bdc8319e4fc7186b3d0bbda8412
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-01 17:19:36 -0400

    Test selected set

--- test/main/TestMapSelection.cpp
+++ test/main/TestMapSelection.cpp
@@ -30,3 +30,37 @@ TEST(TestMapSelection, RemoveElement) {
 
     ASSERT_TRUE(!selection.isSelected(0, 0));
 }
+
+TEST(TestMapSelection, GetSelectedSetWhenOneElement) {
+    MapSelection selection;
+
+    selection.add(0, 0);
+
+    std::set<std::tuple<int, int> > expectedSet = {
+        std::make_tuple(0, 0)
+    };
+    ASSERT_EQ(expectedSet, selection.getSelectedSet());
+}
+
+TEST(TestMapSelection, GetSelectedSetWhenManyElements) {
+    MapSelection selection;
+
+    selection.add(0, 0);
+    selection.add(1, 0);
+
+    std::set<std::tuple<int, int> > expectedSet = {
+        std::make_tuple(0, 0)
+        , std::make_tuple(1, 0)
+    };
+    ASSERT_EQ(expectedSet, selection.getSelectedSet());
+}
+
+TEST(TestMapSelection, GetSelectedSetWhenRemoveElement) {
+    MapSelection selection;
+
+    selection.add(0, 0);
+    selection.remove(0, 0);
+
+    std::set<std::tuple<int, int> > expectedSet = {};
+    ASSERT_EQ(expectedSet, selection.getSelectedSet());
+}
