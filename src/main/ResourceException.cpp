b67aa60e1f546bb3e1a29fbe5542f4b1149718ff
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-01 17:08:59 -0400

    Remove empty constructor in ResourceException

--- src/main/ResourceException.cpp
+++ src/main/ResourceException.cpp
@@ -1,11 +1,5 @@
 #include "ResourceException.h"
 
-ResourceException::ResourceException()
-    : runtime_error("")
-{
-    //
-}
-
 ResourceException::ResourceException(const std::string& message)
     : runtime_error(message)
 {
