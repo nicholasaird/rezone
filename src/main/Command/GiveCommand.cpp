9c4e3825015887b7aa7e4cba1b9d11dc6954cb18
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-02 14:01:33 -0400

    Add GiveCommand

---
+++ src/main/Command/GiveCommand.cpp
@@ -0,0 +1,37 @@
+#include "GiveCommand.h"
+
+GiveCommand::GiveCommand(std::shared_ptr<Map> map, std::shared_ptr<MapSelection> selection)
+    : MapCommand(map)
+    , selection(selection)
+{
+    //
+}
+
+void GiveCommand::execute(CommandArguments& commandArgs) {
+    try {
+        executeThrows(commandArgs);
+    }
+    catch (UnfoundArgumentException e) {
+        std::cout << e.what() << std::endl;
+
+        return;
+    }
+}
+
+void GiveCommand::executeThrows(CommandArguments& commandArgs) {
+    bool all = commandArgs.getBool("--all");
+
+    if(all) {
+        for(const auto& coord : selection->getSelectedSet()) {
+            std::shared_ptr<Zone> zone = map->getZone(coord);
+            zone->provide(zone->getUnprovided());
+        }
+    }
+    else{
+        std::cout << "Unimplemented" << std::endl;
+    }
+}
+
+std::string GiveCommand::getUsage() {
+    return usage;
+}
---
+++ src/main/Command/GiveCommand.h
@@ -0,0 +1,37 @@
+#ifndef GIVECOMMAND_HPP
+#define GIVECOMMAND_HPP
+
+#include <memory>  // shared_ptr
+#include <string>
+
+#include "CommandArguments.h"
+#include "Map.h"
+#include "MapCommand.h"
+#include "MapSelection.h"
+
+class GiveCommand : public MapCommand {
+private:
+    std::string usage =
+R"(Usage:
+    give (<resource> <amount>)...
+    give --all
+
+Options:
+    -a --all  Give all needed resources
+
+Resources:
+    COM_PRODUCT
+    ELECTRICITY
+    IND_PRODUCT
+    PERSON
+)";
+    std::shared_ptr<MapSelection> selection;
+
+public:
+    GiveCommand(std::shared_ptr<Map> map, std::shared_ptr<MapSelection> selection);
+    void execute(CommandArguments& commandArgs) override;
+    void executeThrows(CommandArguments& commandArgs);
+    std::string getUsage() override;
+};
+
+#endif
