317e5bec6953af7d69a6f20686a857122c7e3ce8
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-05 12:07:15 -0400

    Fix local build to touch CMakeList.txt
    Otherwise, build errors occur when you add new files, because they aren't automatically added to the build.

--- localBuildScript.sh
+++ localBuildScript.sh
@@ -4,9 +4,10 @@ export BUILD_TYPE=Debug
 if [ ! -d build ]; then
     mkdir build
     cd build
-    cmake -DCMAKE_CXX_COMPILER=${COMPILER} -DCAMKE_BUILD_TYPE=${BUILD_TYPE} -GNinja ..
+    cmake -DCMAKE_CXX_COMPILER=${COMPILER} -DCMAKE_BUILD_TYPE=${BUILD_TYPE} -GNinja ..
     cd ..
 fi
 
+touch CMakeLists.txt
 cd build
 ninja
