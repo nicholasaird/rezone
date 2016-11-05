72bed101529ff109be575fb4c2600f1810918468
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-05 11:30:39 -0400

    Use ninja for local build

--- localBuildScript.sh
+++ localBuildScript.sh
@@ -1,4 +1,12 @@
 export COMPILER=clang++-3.9
-export RELEASE=Debug
-export COVERAGE=off
-./travisScript.sh
+export BUILD_TYPE=Debug
+
+if [ ! -d build ]; then
+    mkdir build
+    cd build
+    cmake -DCMAKE_CXX_COMPILER=${COMPILER} -DCAMKE_BUILD_TYPE=${BUILD_TYPE} -GNinja ..
+    cd ..
+fi
+
+cd build
+ninja
