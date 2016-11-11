74ef20a7b50dafee84a3e350105c98a4a44dc823
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-11 15:17:34 -0500

    Fix test

--- src/main/Graphics/TextImage.cpp
+++ src/main/Graphics/TextImage.cpp
@@ -1,8 +1,16 @@
 #include "TextImage.h"
 
+#include <string>
+
+#include "ImageException.h"
+
 TextImage::TextImage(int width, int height)
     : width(width)
     , height(height)
 {
-    //
+    if(width <= 0 || height <= 0) {
+        throw ImageException("Dimensions must be greater than zero: ("
+            + std::to_string(width) + ", "
+            + std::to_string(height) + ")");
+    }
 }
