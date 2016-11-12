e457f33098808c89af61e84231c44755f1c51a7b
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-12 18:57:05 -0500

    Add default constructor, move, copy, destructor...

--- src/main/math/Dim2.h
+++ src/main/math/Dim2.h
@@ -6,7 +6,13 @@
 struct Dim2 {
     int x;
     int y;
+
     Dim2();
+    Dim2(const Dim2& other) = default;
+    Dim2& operator=(const Dim2& other) = default;
+    Dim2& operator=(Dim2&& other) noexcept = default;
+    ~Dim2() = default;
+
     Dim2(int x, int y);
 };
 
