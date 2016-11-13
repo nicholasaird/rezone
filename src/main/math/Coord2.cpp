f6f5154c0d5381aca2fe8513501cebd741338985
Nicholas Aird <nicholas.aird@mail.mcgill.ca>
2016-11-12 19:17:35 -0500

    Add default move, copy, destructor, ...

--- src/main/math/Coord2.cpp
+++ src/main/math/Coord2.cpp
@@ -1,5 +1,23 @@
 #include "Coord2.h"
 
+#include "CoordException.h"
+
+Coord2::Coord2()
+    : x(0)
+    , y(0)
+{
+    //
+}
+
+Coord2::Coord2(int x, int y)
+    : x(x)
+    , y(y)
+{
+    if(x < 0 || y < 0) {
+        throw CoordException(x, y, "Values must be greater or equal to zero");
+    }
+}
+
 std::ostream& operator<<(std::ostream& stream, Coord2 coord) {
     stream << "(" << coord.x << ", " << coord.y << ")";
 
--- src/main/math/Coord2.h
+++ src/main/math/Coord2.h
@@ -6,8 +6,13 @@
 struct Coord2 {
     int x;
     int y;
-    Coord2() : x(0), y(0) {/* */};
-    Coord2(int x, int y) : x(x), y(y) {/* */};
+    Coord2();
+    Coord2(const Coord2& other) = default;
+    Coord2& operator=(const Coord2& other) = default;
+    Coord2& operator=(Coord2&& other) noexcept = default;
+    ~Coord2() = default;
+
+    Coord2(int x, int y);
 };
 
 std::ostream& operator<<(std::ostream& stream, Coord2 coord);
